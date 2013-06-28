9dbb49fd9eaec59a82010d8e54a39da3a752b902
James Nylen <jnylen@gmail.com>
2013-06-27 21:09:29 -0400

    Update watch.sh to use inotifywait
    dnotify is obsolete, apparently

--- watch.sh
+++ watch.sh
@@ -2,4 +2,9 @@
 
 cd "$(dirname "$0")"
 
-dnotify -MDRr . -e sh -c 'touch apache/money.wsgi && echo "Updated `date`"'
+while true; do
+    if inotifywait -r -e create,modify,delete,move --exclude '^\./(lib|\.git)/' . ; then
+        touch apache/money.wsgi
+        echo "Updated `date`"
+    fi
+done
